b1f95a0970565bb61b74bfdda84c776e8901f451
erchuan.ma <450595468@qq.com>
2018-06-10 17:21:44 +0800

    init bytom first commit

---
+++ app/src/main/java/bytom/io/BytomActivity.java
@@ -0,0 +1,13 @@
+package bytom.io;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BytomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bytom);
+    }
+}
