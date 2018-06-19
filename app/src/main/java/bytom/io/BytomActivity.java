bd5e838ce0d0f721157f673d642f36423345e1f0
joshuabond <82686929@qq.com>
2018-06-20 01:15:39 +0800

    add wallet list and import wallet activity
    add wallet list and import wallet activity

--- app/src/main/java/bytom/io/BytomActivity.java
+++ app/src/main/java/bytom/io/BytomActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import bytom.io.activity.WalletListActivity;
 import bytom.io.my.transaction.activity.TransactionListActivity;
 
 import bytom.io.assetmanage.AssetManagementActivity;
@@ -29,6 +30,14 @@ public class BytomActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AssetManagementActivity.startActivity(BytomActivity.this);
+            }
+        });
+		
+		findViewById(R.id.bt_wallet).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(BytomActivity.this, WalletListActivity.class);
+                startActivity(intent);
             }
         });
     }
