ed6bba28052bb647b95abaf88d5e75fa2aab6c5a
joshuabond <82686929@qq.com>
2018-06-21 04:51:57 +0800

    add dummy data for wallet list
    add dummy data for wallet list

---
+++ app/src/main/java/bytom/io/activity/PrivateWalletActivity.java
@@ -0,0 +1,31 @@
+package bytom.io.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.TextView;
+
+import bytom.io.R;
+
+public class PrivateWalletActivity extends Activity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_private_wallet);
+        init();
+    }
+    private void init(){
+        findViewById(R.id.iv_left).setOnClickListener(listener);
+        ((TextView)findViewById(R.id.tv_title)).setText(getString(R.string.private_wallet));
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
+
+}
--- app/src/main/java/bytom/io/activity/WalletListActivity.java
+++ app/src/main/java/bytom/io/activity/WalletListActivity.java
@@ -1,29 +1,21 @@
 package bytom.io.activity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ExpandableListView;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-
 import bytom.io.R;
-import bytom.io.entity.transaction.TransListGroupEntity;
-import bytom.io.entity.transaction.TransListItemEntity;
-import bytom.io.my.transaction.adapter.TransactionListAdapter;
 
-
-
-public class WalletListActivity extends Activity{
-
-    private ExpandableListView mExpandableListView;
-    private TransactionListAdapter mAdapter;
-    private ArrayList<TransListGroupEntity> mGroupList;
-    private ArrayList<ArrayList<TransListItemEntity>> mChildGroupList;
-    private ArrayList<TransListItemEntity> mItemList;
+public class WalletListActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,6 +49,9 @@ public class WalletListActivity extends Activity{
                 }
             }
         });
+        WalletAdapter adapter = new WalletAdapter(WalletListActivity.this,R.layout.wallet_item);
+        ListView lv = (ListView)findViewById(R.id.wallet_list);
+        lv.setAdapter(adapter);
     }
 
     View.OnClickListener listener = new View.OnClickListener() {
@@ -65,4 +60,40 @@ public class WalletListActivity extends Activity{
             finish();
         }
     };
+
+
+    public class WalletAdapter extends ArrayAdapter{
+
+        private int mResId = 0;
+
+        public WalletAdapter(Context context, int ResId) {
+            super(context,ResId,new Object[10]);
+            mResId = ResId;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = LayoutInflater.from(getContext()).inflate(mResId,null);
+            ImageView Walleticon = (ImageView) view.findViewById(R.id.wallet_icon);
+            ImageView EnterDetail = (ImageView) view.findViewById(R.id.enter_detail);
+            TextView PrivateWallet = (TextView) view.findViewById(R.id.private_wallet);
+            TextView WalletAddress = (TextView) view.findViewById(R.id.wallet_address);
+            Walleticon.setImageResource(R.drawable.wallet);
+            EnterDetail.setImageResource(R.drawable.arrow_right);
+            PrivateWallet.setText(R.string.private_wallet);
+            WalletAddress.setText("BTMdSDSADSADASDSADASDSAD");
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try{
+                        Intent intent = new Intent(WalletListActivity.this, PrivateWalletActivity.class);
+                        startActivity(intent);
+                    }catch (Exception ex){
+                        ex.printStackTrace();
+                    }
+                }
+            });
+            return view;
+        }
+    }
 }
